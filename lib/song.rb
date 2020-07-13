6190cceb66b89d48e25494d8cb92af2c13f2dade
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-13 14:35:50 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,29 @@ class Song
     self.class.all << self
   end
 
+  def create
+    save
+  end
+
+  def new_by_name(name)
+    @name=name
+    save
+  end
+
+  def self.find_by_name(name)
+    @@all.each do |s|
+      if s.name==name
+        return s
+      end
+    return nil
+  end
+
+  def self.find_or_create_by_name(name)
+    if find_by_name(name)==nil
+      create_by_name(name)
+    else
+      return find_by_name(name)
+    end
+  end
+
 end
